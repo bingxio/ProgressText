e17f70eb631062cb585719a90bf25b685ab7c264
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-05-21 20:20:38 +0800

    clean code

--- android-progress-text/src/main/java/cn/xyiio/progresstext/view/ProgressText.java
+++ android-progress-text/src/main/java/cn/xyiio/progresstext/view/ProgressText.java
@@ -12,30 +12,21 @@ import cn.xyiio.progresstext.R;
 
 /**
  * Created by turaiiao on 18-2-7 11:21.
- * @author turaiiao
- * @version 0.0.4
- * @email 1171840237@qq.com
- * @url https://github.com/turaiiao/progresstext
  */
-
 public class ProgressText extends RelativeLayout {
-    /* 暂停时间毫秒 */
+    
     private int STOP_MILLIS;
 
-    /* 默认文字颜色 */
     public static int DEFAULT_TEXT_COLOR = Color.parseColor("#8A000000");
 
-    /* 默认进度文字颜色 */
     public static int DEFAULT_START_COLOR = Color.parseColor("#E91E63");
 
-    /* 默认字体大小 */
     public static int HEAD_TEXT_SIZE = 11;
     public static int START_TEXT_SIZE = 25;
     public static int END_TEXT_SIZE = 11;
     public static int LINE_TEXT_SIZE = 11;
     public static int BOTTOM_TEXT_SIZE = 11;
 
-    /* 默认分割线 */
     public static String DEFAULT_LINE_TEXT = "/";
 
     private TextView headTextView;
@@ -52,6 +43,7 @@ public class ProgressText extends RelativeLayout {
 
     public ProgressText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        
         LayoutInflater.from(context).inflate(R.layout.layout_progress_text, this);
 
         init();
@@ -76,8 +68,6 @@ public class ProgressText extends RelativeLayout {
         lineTextView.setTextSize(LINE_TEXT_SIZE);
         endTextView.setTextSize(END_TEXT_SIZE);
     }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void setStartText(int progress) {
         startTextView.setText("" + progress);
@@ -104,7 +94,8 @@ public class ProgressText extends RelativeLayout {
         invalidate();
     }
 
-    public void setTextAll(String head_text, int start_progress, int end_progress, String line_text, String bottom_text) {
+    public void setTextAll(String head_text, int start_progress, int end_progress, 
+                           String line_text, String bottom_text) {
         headTextView.setText(head_text);
         startTextView.setText(start_progress);
         endTextView.setText(end_progress);
@@ -112,8 +103,6 @@ public class ProgressText extends RelativeLayout {
         bottomTextView.setText(bottom_text);
         invalidate();
     }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void setHeadTextColor(int color) {
         headTextView.setTextColor(color);
@@ -146,52 +135,51 @@ public class ProgressText extends RelativeLayout {
         endTextView.setTextColor(end_color);
         lineTextView.setTextColor(line_color);
         bottomTextView.setTextColor(bottom_color);
+        
         invalidate();
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
     public void setHeadTextSize(float size) {
         headTextView.setTextSize(size);
+        
         invalidate();
     }
 
     public void setStartTextSize(float size) {
         startTextView.setTextSize(size);
+        
         invalidate();
     }
 
     public void setEndTextSize(float size) {
         endTextView.setTextSize(size);
+        
         invalidate();
     }
 
     public void setLineTextSize(float size) {
         lineTextView.setTextSize(size);
+        
         invalidate();
     }
 
     public void setBottomTextSize(float size) {
         bottomTextView.setTextSize(size);
+        
         invalidate();
     }
 
-    public void setTextSizeAll(float head_size, float start_size, float end_size, float line_size, float bottom_size) {
+    public void setTextSizeAll(float head_size, float start_size, float end_size, 
+                               float line_size, float bottom_size) {
         headTextView.setTextSize(head_size);
         startTextView.setTextSize(start_size);
         endTextView.setTextSize(end_size);
         lineTextView.setTextSize(line_size);
         bottomTextView.setTextSize(bottom_size);
+        
         invalidate();
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /**
-     * 开启文字动画，如果大于 20，转换速度变快
-     * @param start_size 起始文字
-     * @param end_size 结束文字
-     */
     public void startSetTextAnimation(final int start_size, final int end_size) {
         if (start_size > 20)
             STOP_MILLIS = 1000;
@@ -199,6 +187,7 @@ public class ProgressText extends RelativeLayout {
             STOP_MILLIS = 2000;
 
         ValueAnimator anim = ValueAnimator.ofInt(1, start_size);
+        
         anim.setDuration(STOP_MILLIS);
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
@@ -210,17 +199,13 @@ public class ProgressText extends RelativeLayout {
                 endTextView.setText("" + end_size);
             }
         });
+        
         anim.start();
     }
 
-    /**
-     * 开启文字动画，自定义动画速度
-     * @param start_size 起始文字
-     * @param end_size 结束文字
-     * @param duration 动画速度
-     */
     public void startSetTextAnimationWithDuration(final int start_size, final int end_size, final int duration) {
         ValueAnimator anim = ValueAnimator.ofInt(1, start_size);
+        
         anim.setDuration(duration);
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
@@ -232,17 +217,10 @@ public class ProgressText extends RelativeLayout {
                 endTextView.setText("" + end_size);
             }
         });
+        
         anim.start();
     }
-
-    // version 0.0.4 add :
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /**
-     * 开启文字动画，从零起
-     * @param start_size 起始文字
-     * @param end_size 结束文字
-     */
+    
     public void startSetTextAnimationWithNumber(final int start_size, final int end_size) {
         if (start_size > 20)
             STOP_MILLIS = 1000;
@@ -250,6 +228,7 @@ public class ProgressText extends RelativeLayout {
             STOP_MILLIS = 2000;
 
         ValueAnimator anim = ValueAnimator.ofInt(0, start_size);
+        
         anim.setDuration(STOP_MILLIS);
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
@@ -261,17 +240,13 @@ public class ProgressText extends RelativeLayout {
                 endTextView.setText("" + end_size);
             }
         });
+        
         anim.start();
     }
 
-    /**
-     * 开启文字动画，从零起，自定义动画速度
-     * @param start_size 起始文字
-     * @param end_size 结束文字
-     * @param duration 动画速度
-     */
     public void startSetTextAnimationWithNumberAndDuration(final int start_size, final int end_size, int duration) {
         ValueAnimator anim = ValueAnimator.ofInt(0, start_size);
+        
         anim.setDuration(duration);
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
@@ -283,18 +258,13 @@ public class ProgressText extends RelativeLayout {
                 endTextView.setText("" + end_size);
             }
         });
+        
         anim.start();
     }
-
-    /**
-     * 开启文字动画，起始文字增加动画，自定义动画速度
-     * @param start_size 起始文字
-     * @param end_size 结束文字
-     * @param add_size 增加进度
-     * @param duration 动画速度
-     */
+    
     public void setStartTextAddNumber(int start_size, int add_size, final int end_size, int duration) {
         ValueAnimator anim = ValueAnimator.ofInt(0, start_size + add_size);
+        
         anim.setDuration(duration);
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
@@ -306,6 +276,7 @@ public class ProgressText extends RelativeLayout {
                 endTextView.setText("" + end_size);
             }
         });
+        
         anim.start();
     }
 }
